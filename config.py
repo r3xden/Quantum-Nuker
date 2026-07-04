3151f3cb928225372328c183677d2278cc5951c7
Rexden <156419201+r3xden@users.noreply.github.com>
2026-07-04 12:08:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+TOOL_NAME = "Quantum Nuker"
+DEFAULT_ICON_URL = "YOUR_NUKER_ICON_URL_HERE"
+OWNER_MENTION = "@everyone and @here"
+NUKER_CREDIT = "Rexden"
+
+SPAM_CHANNEL_NAMES = [
+    "pussyeater", "fuckedup", "nuker", "hahaha", "fuckyou", 
+    "saymyname", "quantumnuker", "shit", "bich", "void"
+]
+NUM_TO_CREATE_DEFAULT = 50
+
+PING_MESSAGE = f"Hahaha! Nuked by {TOOL_NAME}, Created by {NUKER_CREDIT} - https://github.com/r3xden"
+MESSAGES_PER_BATCH = 1000
+PING_INTERVAL_SECONDS = 5
+
+MAX_RETRY_ATTEMPTS = 3
+INITIAL_DELAY_BETWEEN_ACTIONS = 0.1
